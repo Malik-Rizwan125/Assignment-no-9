82644bdd2efef01ddba20ad09a3d3425466d419a
Malik-Rizwan125 <99021298+Malik-Rizwan125@users.noreply.github.com>
2022-10-18 22:25:33 +0500

    question 1 is complete

---
+++ assignment-no-9.js
@@ -0,0 +1,16 @@
+// Qestion1:
+// 1. Write a TypeScript program to list the properties of a TypeScript object
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12
+//  };
+// console.log(student.name)
+// console.log(student.sclass)
+// console.log(student.rollno)
+// Qestion2:
