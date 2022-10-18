f9d47d7853b61c1d3797afbaf0eb99e4d8583bba
Malik-Rizwan125 <99021298+Malik-Rizwan125@users.noreply.github.com>
2022-10-18 23:23:07 +0500

    4question is done

--- assignment-no-9.js
+++ assignment-no-9.js
@@ -14,3 +14,78 @@
 // console.log(student.sclass)
 // console.log(student.rollno)
 // Qestion2:
+// 2. Write a TypeScript program to delete the rollno property from the following object. Also print the object before or after deleting the property./////incomplete/////
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// interface boys {
+//     name:string,
+//     sclass:string,
+//     rollno?:number,
+// }
+// var student:boys={
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12,
+// }
+//     console.log(student.name);
+// console.log(student.sclass);
+// delete student.rollno;
+// Question 3:
+// 3. Write a TypeScript program to get the length of a TypeScript object.   /////incomplete/////
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// interface student {
+// name : string,
+// sclass : string,
+// rollno : number ,
+// };
+// var obj:student = {
+//     name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 
+// };
+// // 👇️  length: number
+// var length = Object.keys(obj).length;
+// console.log(length)
+// const length = object.keys(obj).length;
+// Question 4:
+// 4. Write a TypeScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.    /////incomplete/////
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
+// var library = [ 
+//     {
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Suzanne Collins',
+//         title:  'Mockingjay: The Final Book of The Hunger Games', 
+//         readingStatus: false
+//     }];
+//     console.log(library.readingstatus)
